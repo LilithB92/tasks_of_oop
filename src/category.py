be9052684b9a9cecf37868ec09daa2f048fcfe11
Lilith <bichakhchyanlilith1992@gmail.com>
2025-11-09 21:04:40 +0300

    add class Category with __init__ function

---
+++ src/category.py
@@ -0,0 +1,18 @@
+class Category:
+    """
+    Класс для представления категории
+    """
+
+    name: str
+    description: str
+    products: list
+    category_count: int = 0
+    product_count: int = 0
+
+    def __init__(self, name, description, products=None) -> None:
+        """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра."""
+        self.name = name
+        self.description = description
+        self.products = products if products else []
+        Category.category_count += 1
+        Category.product_count += len(products) if products else 0
