f75ab21685abab3864b78e745051204244a371ff
Lilith <bichakhchyanlilith1992@gmail.com>
2025-11-10 00:32:05 +0300

    added test for function src.utils.read_json

---
+++ tests/test_utils.py
@@ -0,0 +1,14 @@
+import json
+import os
+from pathlib import Path
+
+from src.utils import read_json
+
+
+def test_json_resd():
+    directory_name = Path(__file__).resolve().parent.parent
+    file_path = os.path.join(directory_name, "data", "test.json")
+    data = {"key": "expected_value"}
+    with open(file_path, "w", encoding="utf-8") as f:
+        json.dump(data, f)
+    assert read_json("test")["key"] == "expected_value"
