669132dac9c6732ac3bfe1dabf9d17048f4e51bb
Lilith <bichakhchyanlilith1992@gmail.com>
2025-11-23 10:19:45 +0300

    added __init for class ProductItreator

--- src/product_iterator.py
+++ src/product_iterator.py
@@ -1,9 +1,18 @@
 from src.category import Category
+
+
 class ProductIterator:
     """
     Класс перебирает товары одной категории
     """
-    user_obj: Category
-    index_obj : int=0
 
+    category_obj: Category
+    index_obj: int = 0
 
+    def __init__(self, category_obj):
+        """
+        Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра.
+
+        :param category_obj: Объект класса Category
+        """
+        self.category_obj = category_obj
