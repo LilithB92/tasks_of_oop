64169ef278cefc972326f933ad707ca6f3b40e69
Lilith <bichakhchyanlilith1992@gmail.com>
2025-11-29 21:24:36 +0300

    advanced __init__ function for smartphone

--- src/smartphone.py
+++ src/smartphone.py
@@ -2,4 +2,21 @@ from src.product import Product
 
 
 class Smartphone(Product):
-    pass
+    """
+    Класс для представления Смартфонов
+    """
+
+    efficiency: float
+    model: str
+    memory: int
+    color: str
+
+    def __init__(self, name, description, price, quantity, efficiency, model, memory, color):
+        """
+        Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра.
+        """
+        super().__init__(name, description, price, quantity)
+        self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
