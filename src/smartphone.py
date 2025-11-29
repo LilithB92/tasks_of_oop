d8beaaacbfda7f4e32960377cb757bf1301d49e3
Lilith <bichakhchyanlilith1992@gmail.com>
2025-11-29 21:40:41 +0300

    added docstring for __init__ smartphone

--- src/smartphone.py
+++ src/smartphone.py
@@ -13,7 +13,15 @@ class Smartphone(Product):
 
     def __init__(self, name, description, price, quantity, efficiency, model, memory, color):
         """
-        Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра.
+          Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра.
+        :param name: Имя смартфона
+        :param description: описание смартфона
+        :param price: цена смартфона
+        :param quantity: количество смартфона
+        :param efficiency: производительность смартфона
+        :param model: модель смартфона
+        :param memory: объем встроенной памяти смартфона
+        :param color: цвет смартфона
         """
         super().__init__(name, description, price, quantity)
         self.efficiency = efficiency
