6ac647602b1353cea2a427e28e8ef643095f8777
Lilith <bichakhchyanlilith1992@gmail.com>
2025-11-29 21:36:07 +0300

    advanced  __init__ for lawn_grass

--- src/lawn_grass.py
+++ src/lawn_grass.py
@@ -2,4 +2,25 @@ from src.product import Product
 
 
 class LawnGrass(Product):
-    pass
+    """Класс для представления Трава газонная"""
+
+    country: str
+    germination_period: str
+    color: str
+
+    def __init__(self, name, description, price, quantity, country, germination_period, color):
+        """
+         Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра.
+
+        :param name: Имя газона
+        :param description: описание газона
+        :param price: цена газона
+        :param quantity: количество газона
+        :param country:страна-производитель газона
+        :param germination_period: срок прорастания газона
+        :param color: цвет газона
+        """
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
