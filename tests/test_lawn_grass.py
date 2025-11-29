d7b6a24842fee5900a4c85342528e6e2dbf22859
Lilith <bichakhchyanlilith1992@gmail.com>
2025-11-29 23:45:09 +0300

    added  tests for __add__ in class lawn_grass

--- tests/test_lawn_grass.py
+++ tests/test_lawn_grass.py
@@ -10,3 +10,11 @@ def test_lawn_grass_init(grass1: object) -> None:
     assert grass1.germination_period == "7 дней"
     assert grass1.color == "Зеленый"
 
+
+def test_lawn_grass_add(grass1: object, grass2: object) -> None:
+    assert grass2 + grass1 == 16750.0
+
+
+def test_lawn_grass_add_error(grass1: object, smartphone1: object) -> None:
+    with pytest.raises(TypeError):
+        assert grass1 + smartphone1
