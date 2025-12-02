5963b3fd1da917b926450f83c13e3f524099a4ef
Lilith <bichakhchyanlilith1992@gmail.com>
2025-12-02 17:11:21 +0300

    added __init__ in class Order

--- src/order.py
+++ src/order.py
@@ -1,6 +1,19 @@
 class Order:
     """
-    Класс для представления
+    Класс для представления купленный товар, количество купленного товара, а также итоговая стоимость.
     """
 
-    pass
+    name: str
+    price: float
+    quantity: int
+
+    def __init__(self, name, price, quantity):
+        """
+         Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра.
+        :param name: Имя заказанного товара
+        :param price: цена заказанного товара
+        :param quantity: количество заказанного товара
+        """
+        self.name = name
+        self.price = price
+        self.quantity = quantity
