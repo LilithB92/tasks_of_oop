76a5baf0e35478f3ea03cd60cd6b1fc51114d0b9
Lilith <bichakhchyanlilith1992@gmail.com>
2025-12-02 19:37:27 +0300

    added test_get_info in tests/test_order

--- tests/test_order.py
+++ tests/test_order.py
@@ -1,4 +1,12 @@
+from src.order import Order
+
+
 def test_product_init(order: object) -> None:
     assert order.name == "Xiaomi Redmi Note 11"
     assert order.price == 31000.0
     assert order.quantity == 14
+
+
+def test_get_info() -> None:
+    order = Order(name="Xiaomi Redmi Note 11", price=31000.0, quantity=14)
+    assert order.get_info() == "Заказ: Xiaomi Redmi Note 11 x 14, Итоговая стоимость: 434000.0"
