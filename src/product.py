a602fc86ed68fbe14ea40282153941cb593b22cd
Lilith <bichakhchyanlilith1992@gmail.com>
2025-12-03 13:39:49 +0300

    fixed product without Base ABC

--- src/product.py
+++ src/product.py
@@ -4,7 +4,7 @@ from src.base_product import BaseProduct
 from src.print_mixin import PrintMixin
 
 
-class Product(BaseProduct, ABC, PrintMixin):
+class Product(BaseProduct, PrintMixin):
     """
     Класс для представления продукты электро товаров
     """
